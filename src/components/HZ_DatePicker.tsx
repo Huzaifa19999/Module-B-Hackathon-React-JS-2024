c8037335ad90fa9567877f5ea126e62360dbfeb5
Huzaifa19999 <huzaifabhai10@gmail.com>
2024-08-10 11:09:50 +0500

    Hackathon-2024

---
+++ src/components/HZ_DatePicker.tsx
@@ -0,0 +1,32 @@
+type propsType = {
+    datasource: any[],
+    gridCols: {
+        key: string,
+        label: string,
+        displayField?: any
+      }[],
+      loading?: boolean
+      className:string,
+  }
+  
+   function HZ_DataGrid(props: propsType) {
+  
+    const { datasource, gridCols, loading, className } = props
+  
+    return <>
+        {loading ? <h1>Loading ...</h1> : <table className={className}>
+            <thead>
+                {gridCols.map((col, ind) => <th key={ind}>{col.label}</th>)}
+            </thead>
+            <tbody>
+                {datasource.map((rows: any) => <tr>
+                    {gridCols.map((col, ind) => <td
+                        key={ind}
+                    >{col.displayField ? col.displayField(rows) : rows[col.key]}</td>)}
+                </tr>)}
+            </tbody>
+        </table>}
+    </>
+  }
+  
+  export default HZ_DataGrid
